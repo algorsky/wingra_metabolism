efab9a8450637998bcede879ec37e2b8514f3840
Robert <ladwigjena@gmail.com>
2023-10-19 12:44:13 -0500

    weird

--- src/run_model.R
+++ src/run_model.R
@@ -8,13 +8,46 @@ setwd("~/Documents/Adrianna/wingra_metabolism/")
 logger <- read.csv("data/january/deep_DO.csv")
 meteorology <- read.csv('data/january/weather.csv')
 
+# fake data
+fake = data.frame(datetime =seq(as.POSIXct("2014-06-16 00:00:00", tz="GMT"),
+                                  as.POSIXct("2014-06-17 23:55:00", tz="GMT"), length.out=288*2),
+                  do.obs = 2*sin(2*pi*(1/288)*(1:(288*2))+1.1*pi) + 8 + rnorm(288*2, 0, 0.5),
+                  wtr = 3*sin(2*pi*(1/288)*(1:(288*2))+pi) + 17 + rnorm(288*2, 0, 0.15),
+                  do.sat =LakeMetabolizer::o2.at.sat.base(wtr, 960),
+                  irr =(1500*sin(2*pi*(1/288)*(1:(288*2))+1.5*pi) +650 + rnorm(288*2, 0, 0.25)) *
+                    ifelse(is.day(datetime, 42.3), 1, 0),
+                  k.gas = 0.4,
+                  z.mix = 1)
+
+ggplot(reshape2::melt(fake, id = 'datetime')) +
+  geom_line(aes(datetime, value)) +
+  facet_wrap(~ variable, scales = 'free') +
+  theme_minimal()
+
+metab.bay <- metab(fake, "bayesian", lake.lat=42.6)
+
+ggplot(metab.bay) +
+  geom_line(aes(doy, GPP, col = "GPP")) +
+  geom_line(aes(doy, NEP, col = 'NEP')) +
+  geom_line(aes(doy, R, col = "R")) +
+  theme_minimal()
+
+# ADRIANNAS DATA OF LAKE WINGRA
 df = merge(logger, meteorology, by = 'datetime') %>%
+  mutate(wind_speed_ms = wind_speed_ms * 10**(-4),
+         solar_flux_Wm2 = solar_flux_Wm2 * 0.5) %>%
   mutate(datetime = lubridate::ymd_hms(datetime),
          DO_sat = o2.at.sat.base(temp = Temp_C, altitude = 258),
-         z_mix = 2,
+         z_mix = 1,
          k_600 = k.vachon.base(wnd = wind_speed_ms, lake.area = 1.3 * 10**6),
          k_gas = k600.2.kGAS.base(k600 = k_600, temperature =Temp_C, gas = "O2"),
-         PAR = solar_flux_Wm2 * 2.114)
+         PAR = solar_flux_Wm2 * 2.114) %>%
+  mutate(DO_sat_calc = (100 * DO_mgL)/DO_perc)
+
+ggplot(df) +
+  geom_line(aes(datetime, DO_sat, col = 'calculated')) + 
+  geom_line(aes(datetime, DO_sat_calc, col = 'downscaled')) + 
+  theme_minimal()
 
 # SW (W/m2) to PAR (umol/m2/s) is 2.114
 # gpp = p_max * tanh((P_alpha * lux))/ p_max)
@@ -25,8 +58,13 @@ data = df %>%
   rename(do.obs = DO_mgL, do.sat = DO_sat, k.gas = k_gas, z.mix = z_mix, irr = PAR, wtr = Temp_C) %>%
   select(datetime, do.obs, do.sat, k.gas, z.mix, irr, wtr)
 
-m.df = reshape2::melt(df, id = 'datetime')
+m.df = reshape2::melt(data, id = 'datetime')
 ggplot(m.df) +
+  geom_line(aes(datetime, value)) + 
+  facet_wrap(~ variable, scales = 'free') +
+  theme_minimal()
+
+ggplot(m.df %>% filter(datetime <= "2023-01-03 23:45:00")) +
   geom_line(aes(datetime, value)) + 
   facet_wrap(~ variable, scales = 'free') +
   theme_minimal()
@@ -39,7 +77,7 @@ ggplot(data) +
   theme_minimal()
 
 # BAYESIAN
-output = metab(data = data, method = 'bayesian', lake.lat = 43.3)
+output = metab(data = data, method = 'bayesian', lake.lat = 43.3,  wtr.name="wtr", irr.name="irr", do.obs.name="do.obs")
 
 str(output)
  
