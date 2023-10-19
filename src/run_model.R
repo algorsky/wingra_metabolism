a65e26b9835d445a8839ba0e0978efa1569099af
Robert <ladwigjena@gmail.com>
2023-10-19 11:55:50 -0500

    all methods

--- src/run_model.R
+++ src/run_model.R
@@ -11,7 +11,7 @@ meteorology <- read.csv('data/january/weather.csv')
 df = merge(logger, meteorology, by = 'datetime') %>%
   mutate(datetime = lubridate::ymd_hms(datetime),
          DO_sat = o2.at.sat.base(temp = Temp_C, altitude = 258),
-         z_mix = 1,
+         z_mix = 2,
          k_600 = k.vachon.base(wnd = wind_speed_ms, lake.area = 1.3 * 10**6),
          k_gas = k600.2.kGAS.base(k600 = k_600, temperature =Temp_C, gas = "O2"),
          PAR = solar_flux_Wm2 * 2.114)
@@ -31,7 +31,15 @@ ggplot(m.df) +
   facet_wrap(~ variable, scales = 'free') +
   theme_minimal()
 
-output = metab(data = data, method = 'bayesian')
+ggplot(data) +
+  geom_line(aes(datetime, do.obs - do.sat, col = 'diff')) +
+  geom_line(aes(datetime, do.obs, col = 'obs')) +
+  geom_line(aes(datetime, do.sat, col = 'sat')) +
+  geom_hline(yintercept = 0, linetype = 'dashed')+
+  theme_minimal()
+
+# BAYESIAN
+output = metab(data = data, method = 'bayesian', lake.lat = 43.3)
 
 str(output)
  
@@ -40,3 +48,40 @@ ggplot(output) +
   geom_line(aes(doy, NEP, col = 'NEP')) +
   geom_line(aes(doy, R, col = "R")) +
   theme_minimal()
+
+# BOOKKEEPING
+output = metab(data = data, method = 'bookkeep', lake.lat = 43.3)
+
+ggplot(output) +
+  geom_line(aes(doy, GPP, col = "GPP")) +
+  geom_line(aes(doy, NEP, col = 'NEP')) +
+  geom_line(aes(doy, R, col = "R")) +
+  theme_minimal()
+
+# OLS
+output = metab(data = data, method = 'ols', lake.lat = 43.3)
+
+ggplot(output) +
+  geom_line(aes(doy, GPP, col = "GPP")) +
+  geom_line(aes(doy, NEP, col = 'NEP')) +
+  geom_line(aes(doy, R, col = "R")) +
+  theme_minimal()
+
+# MLE
+output = metab(data = data, method = 'mle', lake.lat = 43.3)
+
+ggplot(output) +
+  geom_line(aes(doy, GPP, col = "GPP")) +
+  geom_line(aes(doy, NEP, col = 'NEP')) +
+  geom_line(aes(doy, R, col = "R")) +
+  theme_minimal()
+
+# KALMAN
+output = metab(data = data, method = 'kalman', lake.lat = 43.3)
+
+ggplot(output) +
+  geom_line(aes(doy, GPP, col = "GPP")) +
+  geom_line(aes(doy, NEP, col = 'NEP')) +
+  geom_line(aes(doy, R, col = "R")) +
+  theme_minimal()
+
